9b2f097ea7bdb2dfcc9208ee47f38be6ffc1928e
wael hosni <wael.hosni@poulinagroup.com>
2020-05-03 11:23:34 +0100

    url fiche suivi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
 
 
-  FicheSuiviMicroservice: 'http://localhost:5000/api',
+  // FicheSuiviMicroservice: 'http://localhost:5000/api',
+  FicheSuiviMicroservice: 'http://192.168.160.74:31633/production-fiche-suivi/api',
   //ChaudiereMicroservice: 'https://localhost:44352/api',
   FilialeMicroservice: 'http://192.168.160.74:31633/production-subsidiaries/api',
 
